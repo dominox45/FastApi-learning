e28c90c16156c1635e403f6eed6547f907298876
dominox45 <tolaba.fet@gmail.com>
2026-04-04 15:19:05 -0300

    feat(static): create static/images and configure StaticFiles in main with starkiller.jpg

--- app/main.py
+++ app/main.py
@@ -1,13 +1,20 @@
 from fastapi import FastAPI
 from app.routers import products #importamos el router de products
 from app.routers import users #importamos el router de users
+from fastapi.staticfiles import StaticFiles #importamos StaticFiles para montar la carpeta de archivos estáticos (imagenes, css, js, etc)
 
 app = FastAPI()
+#uvicorn app.main:app --reload 
+# comando de inicio del server, el --reload hace que se reinicie el server cada vez que se hace un cambio en el codigo, 
+# lo cual es muy util durante el desarrollo. El app.main:app hace referencia al archivo main.py y a la instancia de 
+# FastAPI llamada app.
 
 #routers 
 app.include_router(products.router) #incluimos el router de products en nuestra app
 app.include_router(users.router) #incluimos el router de users en nuestra app
 
+# Montar carpeta de archivos estáticos
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
 
 @app.get("/")
